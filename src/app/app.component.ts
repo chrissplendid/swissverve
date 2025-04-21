7e4d72edf27cf2bc6bb28552ce78b5b2a0e763c3
chrissplendid <christianumolu@gmail.com>
2025-04-21 19:33:51 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CountdownModule } from 'ngx-countdown';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CountdownModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
