4505c3f90e7a707795b0a888e02d5d4e35f60a00
Dev Coder <36684405+chrissplendid@users.noreply.github.com>
2025-04-29 11:10:55 +0100

    chore:

--- dist/swissverve/server/angular-app-manifest.mjs
+++ dist/swissverve/server/angular-app-manifest.mjs
@@ -28,13 +28,13 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 6397, hash: '5e59e6d2881ec2a92f57824c1185c3bf79a302091a724afeec9de4f482ad0cb0', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 2341, hash: 'becec79615081d330e90017b12e0a520c70733ed05e39f8c2e1310e435fca1c4', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 22142, hash: '90c1756460c96cddd2706a08c5567212a32dfc0674dfef290ea9941256da3e74', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'register/index.html': {size: 20439, hash: 'f70d02e1262e64f4fd3412c1a3256949bf23ea2d7e7346cae105241b13a23513', text: () => import('./assets-chunks/register_index_html.mjs').then(m => m.default)},
-    'dashboard/internal-transfer/index.html': {size: 33606, hash: '2a3d4d8ee55edc693fd4298f9fe6139652a7966613a83bc159a5d2a985496b34', text: () => import('./assets-chunks/dashboard_internal-transfer_index_html.mjs').then(m => m.default)},
-    'dashboard/deposit/index.html': {size: 33579, hash: 'cd0c6bc832f046cac811d4964259332a5eb9704e0874513185782160b16df5e1', text: () => import('./assets-chunks/dashboard_deposit_index_html.mjs').then(m => m.default)},
-    'dashboard/index.html': {size: 62079, hash: '1d60a0482e33bfb96c3e09068c7b8eb1d77c614ea153ed324216afe38fd41c67', text: () => import('./assets-chunks/dashboard_index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 6496, hash: '929db32c1263ebed871f7727cd3ee97dbf53fd357f937531f315758820cf9440', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 2440, hash: '6ae536bdbc72db841ed40244df767ef95f0b1a06197937df70fa3b2dfc6fe248', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'register/index.html': {size: 20538, hash: '0908489effae669f54de8f2a539dcc9ad95e8564db52d523bebecda7c8ef3af7', text: () => import('./assets-chunks/register_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 22241, hash: '72aff8030e5057f1409ef2ff7f0ecac05febb9b403cfcf53669a2f0da44404c5', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'dashboard/internal-transfer/index.html': {size: 33705, hash: 'e530ca0059fe8bbb533e83f4159f05561a80b8a9f22858b8a319004410ce9e4b', text: () => import('./assets-chunks/dashboard_internal-transfer_index_html.mjs').then(m => m.default)},
+    'dashboard/index.html': {size: 62178, hash: 'e0fd1bec90afd6f2a7d4fb879a59cfe308ee1906a60be81cc10ee5b6f55d58de', text: () => import('./assets-chunks/dashboard_index_html.mjs').then(m => m.default)},
+    'dashboard/deposit/index.html': {size: 33678, hash: 'e4bcf84579cb1ec127cac85a81ef5870c7684fc360106b6b90c5db4508761de5', text: () => import('./assets-chunks/dashboard_deposit_index_html.mjs').then(m => m.default)},
     'styles-LCZRYJJ6.css': {size: 230803, hash: 'srKqEcAGzE4', text: () => import('./assets-chunks/styles-LCZRYJJ6_css.mjs').then(m => m.default)}
   },
 };
