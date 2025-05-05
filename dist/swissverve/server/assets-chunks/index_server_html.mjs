96f4e49c78f7fac57122bbb8ddc93bc739ec4134
Dev Coder <36684405+chrissplendid@users.noreply.github.com>
2025-05-05 08:01:41 +0100

    chore.

---
+++ dist/swissverve/server/assets-chunks/index_server_html.mjs
@@ -0,0 +1,63 @@
+export default `<!doctype html>
+<html lang="en" dir="ltr" data-startbar="dark" data-bs-theme="light">
+
+<head>
+
+
+  <meta charset="utf-8"/>
+  <title>Swissverve | Swissverve - Your Trusted Online Banking System</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta content="Premium Multipurpose Admin &amp; Dashboard Template" name="description"/>
+  <meta content="" name="author"/>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+
+  <!-- App favicon -->
+  <link rel="shortcut icon" href="assets/images/favicon.ico">
+
+
+
+  <!-- <link rel="stylesheet" href="https://swissverve.com/assets/libs/jsvectormap/css/jsvectormap.min.css"> -->
+
+  <!-- App css -->
+  <link href="https://swissverve.com/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+  <link href="https://swissverve.com/assets/css/icons.min.css" rel="stylesheet" type="text/css"/>
+  <link href="https://swissverve.com/assets/css/app.min.css" rel="stylesheet" type="text/css"/>
+
+  <!--  "src/assets/libs/jsvectormap/css/jsvectormap.min.css",
+              "src/assets/css/bootstrap.min.css",
+              "src/assets/css/icons.min.css",
+              "src/assets/css/app.min.css", -->
+
+
+<link rel="stylesheet" href="styles-Y6VAXYGL.css"></head>
+
+<body><script type="text/javascript" id="ng-event-dispatch-contract">(()=>{function p(t,n,r,o,e,i,f,m){return{eventType:t,event:n,targetElement:r,eic:o,timeStamp:e,eia:i,eirp:f,eiack:m}}function u(t){let n=[],r=e=>{n.push(e)};return{c:t,q:n,et:[],etc:[],d:r,h:e=>{r(p(e.type,e,e.target,t,Date.now()))}}}function s(t,n,r){for(let o=0;o<n.length;o++){let e=n[o];(r?t.etc:t.et).push(e),t.c.addEventListener(e,t.h,r)}}function c(t,n,r,o,e=window){let i=u(t);e._ejsas||(e._ejsas={}),e._ejsas[n]=i,s(i,r),s(i,o,!0)}window.__jsaction_bootstrap=c;})();
+</script>
+  <app-root></app-root>
+
+
+  <!-- Javascript  -->
+  <!-- vendor js -->
+
+  <!--<script src="assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>-->
+  <script src="https://swissverve.com/assets/libs/simplebar/simplebar.min.js"></script>
+
+    <script src="https://swissverve.com/assets/libs/apexcharts/apexcharts.min.js"></script>
+    <script src="https://swissverve.com/assets/data/stock-prices.js"></script>
+    <script src="https://swissverve.com/assets/libs/jsvectormap/js/jsvectormap.min.js"></script>
+    <script src="https://swissverve.com/assets/libs/jsvectormap/maps/world.js"></script>
+    <script src="https://swissverve.com/assets/js/pages/index.init.js"></script>
+    <script src="https://swissverve.com/assets/js/pages/ecommerce-index.init.js"></script>
+    <script src="https://swissverve.com/assets/js/app.js"></script>
+
+    <!-- "src/assets/libs/simplebar/simplebar.min.js",
+              "src/assets/libs/apexcharts/apexcharts.min.js",
+              "src/assets/data/stock-prices.js",
+              "src/assets/libs/jsvectormap/js/jsvectormap.min.js",
+              "src/assets/libs/jsvectormap/maps/world.js",
+              "src/assets/js/pages/index.init.js",
+              "src/assets/js/pages/ecommerce-index.init.js",
+              "src/assets/js/app.js" -->
+<script src="polyfills-FFHMD2TL.js" type="module"></script><script src="scripts-CV4GDUE4.js" defer=""></script><script src="main-5VB2KHCX.js" type="module"></script></body>
+
+</html>`;
