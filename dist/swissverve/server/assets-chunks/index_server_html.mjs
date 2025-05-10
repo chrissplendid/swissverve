009b0e0569a4bb4f65d342056c53f58b7b491b8e
Dev Coder <36684405+chrissplendid@users.noreply.github.com>
2025-05-10 14:01:44 +0100

    fix.

--- dist/swissverve/server/assets-chunks/index_server_html.mjs
+++ dist/swissverve/server/assets-chunks/index_server_html.mjs
@@ -2,47 +2,47 @@ export default `<!doctype html>
 <html lang="en" dir="ltr" data-startbar="dark" data-bs-theme="light">
 
 <head>
-  <meta charset="utf-8"/>
+  <meta charset="utf-8">
   <title>Swissverve - Your Trusted Online Banking System</title>
-  <base href="/"/>
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-  <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-  <meta name="keywords" content="mobile banking, business consulting, bank loans, credit cards, finance, insurance, broker business, forex trading"/>
-  <meta name="description" content="Swissverve"/>
-  <meta name="author" content=""/>
+  <base href="/">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+  <!-- SEO Meta -->
+  <meta name="keywords" content="mobile banking, business consulting, bank loans, credit cards, finance, insurance, broker business, forex trading">
+  <meta name="description" content="Swissverve">
+  <meta name="author" content="">
 
   <!-- Social Meta -->
-  <meta property="og:site_name" content="Swissverve"/>
-  <meta property="og:url" content=""/>
-  <meta property="og:type" content="website"/>
-  <meta property="og:title" content="Swissverve - Banking and Finance"/>
-  <meta property="og:image" content="images/assets/ogg.html"/>
+  <meta property="og:site_name" content="Swissverve">
+  <meta property="og:url" content="">
+  <meta property="og:type" content="website">
+  <meta property="og:title" content="Swissverve - Banking and Finance">
+  <meta property="og:image" content="images/assets/ogg.html">
 
   <!-- Theme Colors -->
-  <meta name="theme-color" content="#913BFF"/>
-  <meta name="msapplication-navbutton-color" content="#913BFF"/>
-  <meta name="apple-mobile-web-app-status-bar-style" content="#913BFF"/>
+  <meta name="theme-color" content="#913BFF">
+  <meta name="msapplication-navbutton-color" content="#913BFF">
+  <meta name="apple-mobile-web-app-status-bar-style" content="#913BFF">
 
   <!-- Favicon -->
-  <link rel="shortcut icon" href="assets/images/favicon.ico"/>
+  <link rel="shortcut icon" href="assets/images/favicon.ico">
 
-  <!-- External Libraries -->
-  <link rel="stylesheet" href="https://swissverve.com/assets/libs/jsvectormap/css/jsvectormap.min.css"/>
+  <!-- Core & Vendor CSS -->
+  <link href="https://swissverve.com/assets/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://swissverve.com/assets/css/app.min.css" rel="stylesheet">
+  <link href="/assets/css/icons.min.css" rel="stylesheet">
+  <link href="https://swissverve.com/assets/libs/jsvectormap/css/jsvectormap.min.css" rel="stylesheet">
 
-  <!-- Core App CSS -->
-  <link href="https://swissverve.com/assets/css/bootstrap.min.css" rel="stylesheet"/>
-  <link href="assets/css/icons.min.css" rel="stylesheet"/>
-  <link href="https://swissverve.com/assets/css/app.min.css" rel="stylesheet"/>
-
-  <!-- Theme & Vendor CSS -->
-  <link rel="stylesheet" href="assets/assets/css/satoshi.css"/>
-  <link rel="stylesheet" href="assets/assets/fonts/bootstrap-icons.css"/>
-  <link rel="stylesheet" href="https://swissverve.com/assets/assets/css/animate.min.css"/>
-  <link rel="stylesheet" href="https://swissverve.com/assets/assets/css/slick.css"/>
-  <link rel="stylesheet" href="https://swissverve.com/assets/assets/css/magnific-popup.css"/>
-  <link rel="stylesheet" href="https://swissverve.com/assets/assets/css/aos.css"/>
-  <link rel="stylesheet" href="https://swissverve.com/assets/assets/css/spacing.css"/>
-  <link rel="stylesheet" href="https://swissverve.com/assets/assets/css/main.css"/>
+  <!-- Theme CSS -->
+  <link href="/assets/assets/css/satoshi.css" rel="stylesheet">
+  <link href="/assets/assets/fonts/bootstrap-icons.css" rel="stylesheet">
+  <link href="https://swissverve.com/assets/assets/css/animate.min.css" rel="stylesheet">
+  <link href="https://swissverve.com/assets/assets/css/slick.css" rel="stylesheet">
+  <link href="https://swissverve.com/assets/assets/css/magnific-popup.css" rel="stylesheet">
+  <link href="https://swissverve.com/assets/assets/css/aos.css" rel="stylesheet">
+  <link href="https://swissverve.com/assets/assets/css/spacing.css" rel="stylesheet">
+  <link href="https://swissverve.com/assets/assets/css/main.css" rel="stylesheet">
 <link rel="stylesheet" href="styles-Y6VAXYGL.css"></head>
 
 <body><script type="text/javascript" id="ng-event-dispatch-contract">(()=>{function p(t,n,r,o,e,i,f,m){return{eventType:t,event:n,targetElement:r,eic:o,timeStamp:e,eia:i,eirp:f,eiack:m}}function u(t){let n=[],r=e=>{n.push(e)};return{c:t,q:n,et:[],etc:[],d:r,h:e=>{r(p(e.type,e,e.target,t,Date.now()))}}}function s(t,n,r){for(let o=0;o<n.length;o++){let e=n[o];(r?t.etc:t.et).push(e),t.c.addEventListener(e,t.h,r)}}function c(t,n,r,o,e=window){let i=u(t);e._ejsas||(e._ejsas={}),e._ejsas[n]=i,s(i,r),s(i,o,!0)}window.__jsaction_bootstrap=c;})();
@@ -51,32 +51,22 @@ export default `<!doctype html>
 
   <!-- Core JS -->
   <script src="https://swissverve.com/assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <!--<script src="https://swissverve.com/assets/libs/simplebar/simplebar.min.js"></script>
-  <script src="https://swissverve.com/assets/libs/apexcharts/apexcharts.min.js"></script>
-  <script src="https://swissverve.com/assets/data/stock-prices.js"></script>
-  <script src="https://swissverve.com/assets/libs/jsvectormap/js/jsvectormap.min.js"></script>
-  <script src="https://swissverve.com/assets/libs/jsvectormap/maps/world.js"></script>
-  <script src="https://swissverve.com/assets/js/pages/index.init.js"></script>
-  <script src="https://swissverve.com/assets/js/pages/ecommerce-index.init.js"></script>
-  <script src="https://swissverve.com/assets/js/app.js"></script>-->
 
-  <!-- Theme/Vendor JS -->
+  <!-- Theme & Vendor JS -->
   <script src="https://swissverve.com/assets/assets/js/modernizr-3.5.0.min.js"></script>
   <script src="https://swissverve.com/assets/assets/js/jquery-3.5.1.min.js"></script>
   <script src="https://swissverve.com/assets/assets/js/popper.min.js"></script>
   <script src="https://swissverve.com/assets/assets/js/bootstrap.min.js"></script>
   <script src="https://swissverve.com/assets/assets/js/jquery.meanmenu.js"></script>
-  <!--<script src="https://swissverve.com/assets/assets/js/slick.min.js"></script>-->
   <script src="https://swissverve.com/assets/assets/js/imagesloaded.pkgd.min.js"></script>
   <script src="https://swissverve.com/assets/assets/js/isotope.pkgd.min.js"></script>
   <script src="https://swissverve.com/assets/assets/js/jquery.magnific-popup.min.js"></script>
   <script src="https://swissverve.com/assets/assets/js/wow.min.js"></script>
   <script src="https://swissverve.com/assets/assets/js/aos.js"></script>
-  <!--<script src="https://swissverve.com/assets/assets/js/jquery.counterup.min.js"></script>-->
   <script src="https://swissverve.com/assets/assets/js/jquery.waypoints.min.js"></script>
   <script src="https://swissverve.com/assets/assets/js/jquery.easypiechart.min.js"></script>
   <script src="https://swissverve.com/assets/assets/js/jquery-ui.js"></script>
-  
-<script src="polyfills-FFHMD2TL.js" type="module"></script><script src="scripts-CV4GDUE4.js" defer=""></script><script src="main-45CAY3E7.js" type="module"></script></body>
+<script src="polyfills-FFHMD2TL.js" type="module"></script><script src="scripts-CV4GDUE4.js" defer=""></script><script src="main-EMX6ZOW6.js" type="module"></script></body>
 
-</html>`;
+</html>
+`;
