9e20bde09335e450493b5338d121f97238dbb42c
Dev Coder <36684405+chrissplendid@users.noreply.github.com>
2025-05-13 16:07:37 +0100

    profile feat. color chore.

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { CommunicatorService } from '../communicator.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
@@ -6,6 +7,43 @@ import { Component } from '@angular/core';
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  accountType: any;
+  balance: any;
+  country: any;
+  currency: any;
+  email: any;
+  firstname: any;
+  lastname: any;
+  userID: any;
+  username: any;
+
+  // A CONSTRUCTOR METHOD THAT RUNS BEFORE THE PAGE INITIALIZES
+  constructor(private communicatorService: CommunicatorService) { }
+
+  ngOnInit(): void {
+    /* GET LOGGED IN USER DATA FROM THE SERVER THROUGH A SERVICE METHOD */
+    this.communicatorService.onLoginService().subscribe({
+      next: (res) => {
+        console.log(res.data);
+        if (res.status == true) {
+          this.accountType = res.data.wallet.account_type;
+          this.balance = res.data.wallet.balance;
+          this.country = res.data.profile.country
+          this.currency = res.data.wallet.currency;
+          this.email = res.data.email;
+          this.firstname = res.data.profile.first_name;
+          this.lastname = res.data.profile.last_name;
+          this.userID = res.data.id;
+          this.username = res.data.username;
+
+        } else {
+          alert("Error Getting User Data!");
+        }
+      },
+      error: () => { }
+    })
+
+  }
 
 }
