baa68f0f846141bb7cb00d657e1cb5c7db4c8a3a
Dev Coder <36684405+chrissplendid@users.noreply.github.com>
2025-05-13 12:57:51 +0100

    feat. chore.

--- src/app/transactions/transactions.component.ts
+++ src/app/transactions/transactions.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CommunicatorService } from '../communicator.service';
+import { Component, OnInit  } from '@angular/core';
 
 @Component({
   selector: 'app-transactions',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './transactions.component.html',
   styleUrl: './transactions.component.css'
 })
-export class TransactionsComponent {
+export class TransactionsComponent implements OnInit {
+  transactions: any;
+
+  // A CONSTRUCTOR METHOD THAT RUNS BEFORE THE PAGE INITIALIZES
+  constructor(private communicatorService: CommunicatorService) {}
+
+  ngOnInit(): void {
+
+    /* GET TRANSACTIONS DATA */
+    this.communicatorService.getTransactionsService().subscribe({
+      next: (res) => {
+        console.log(res.data.data);
+        if(res.data) {
+          this.transactions = res.data.data;
+        } else {
+          alert("Error Getting User Data!");
+        }
+      },
+      error: () => {}
+    })
+    
+  }
 
 }
